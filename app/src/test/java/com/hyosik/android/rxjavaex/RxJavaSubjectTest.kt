c697731dbf5d9c238dfa1636e1a629064d863f99
hyosikchoi <huchoi24@naver.com>
2022-06-15 01:17:16 +0900

    Hot stream 속성의 Subject 특성 파악 및 여러 Observable 관련 merge 처리 테스트

--- app/src/test/java/com/hyosik/android/rxjavaex/RxJavaSubjectTest.kt
+++ app/src/test/java/com/hyosik/android/rxjavaex/RxJavaSubjectTest.kt
@@ -1,8 +1,10 @@
 package com.hyosik.android.rxjavaex
 
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.functions.Consumer
 import io.reactivex.rxjava3.subjects.PublishSubject
 import org.junit.Test
+import java.util.concurrent.TimeUnit
 
 class RxJavaSubjectTest {
 
@@ -11,8 +13,36 @@ class RxJavaSubjectTest {
 
         val src = PublishSubject.create<String>()
 
-        src.subscribe(Consumer {  })
+        src.subscribe(
+            {item -> println("A: $item")},
+            {t -> t.printStackTrace()},
+            {println("A: onComplete")}
+        )
+        src.subscribe(
+            {item -> println("B: $item")},
+            {t -> t.printStackTrace()},
+            {println("B: onComplete")}
+        )
+        src.onNext("Hello")
+        src.onNext("World")
+        src.onNext("!!!")
+        src.onComplete()
+        // Subject는 Hot Observable 이라는 사실을 잊으면 안된다.
+    }
 
+    @Test
+    fun `publish subject merge 처리 테스트`() {
+
+        val src1 = Observable.interval(1,TimeUnit.SECONDS)
+        val src2 = Observable.interval(500,TimeUnit.MILLISECONDS)
+
+        val subject = PublishSubject.create<String>()
+        src1.map { item -> "A: $item" }.subscribe(subject)
+        src2.map { item -> "B: $item" }.subscribe(subject)
+        subject.subscribe { item -> println(item)}
+        Thread.sleep(5000)
+        // Subject를 통해 아이템을 재발행도하고, merge 연산자처럼 src1,src2 두 Observable 소스를 묶어서
+        // 이벤트를 subject 하나로 관리하는것도 가능하다.
     }
 
 }
