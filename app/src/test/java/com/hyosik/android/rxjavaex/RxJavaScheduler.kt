e880a1424771946f68a8f5e8addc2b322b15cc71
hyosikchoi <huchoi24@naver.com>
2023-12-26 02:28:41 +0900

    Add - SubscribeOn and observeOn compare

--- app/src/test/java/com/hyosik/android/rxjavaex/RxJavaScheduler.kt
+++ app/src/test/java/com/hyosik/android/rxjavaex/RxJavaScheduler.kt
@@ -3,6 +3,8 @@ package com.hyosik.android.rxjavaex
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.PublishSubject
+import kotlinx.coroutines.delay
 import org.junit.Test
 
 class RxJavaScheduler {
@@ -46,6 +48,30 @@ class RxJavaScheduler {
         // computation Scheduler 는 Coroutine 에서 Default Dispatcher 와 같다고 보면 된다. (계산 적인 작업에 사용)
 
         /** 실제 작업에선 네트워크 작업으로 구독은 io로 발행(observeOn)은 UI갱신은 main Thread 에서 수행하게끔 한다. */
+    }
+
+    @Test
+    fun `subscribeOn 과 observeOn 연산자 테스트2`() {
+        /** onNext 로 발행을 하기 전까지는 io thread 에서 동작을 하고 */
+        /** 발행을 시작하면 computation thread 로 컨텍스트 스위칭을 한다. */
+        val os: Observable<Int> = Observable.create { emitter ->
+            (0..2).forEach {i ->
+                val threadName = Thread.currentThread().name
+                println("#Subs on $threadName : $i")
+                emitter.onNext(i)
+                Thread.sleep(100)
+            }
+            emitter.onComplete()
+        }
+
+        os.observeOn(Schedulers.computation())
+            .subscribeOn(Schedulers.io())
+            .subscribe { i ->
+                val threadName = Thread.currentThread().name
+                println("#Obs on $threadName : $i")
+            }
+        // RXJava 는 비동기 데이터 스트림이므로 Thread sleep 을 걸어줘야 비동기로 그동안에 데이터를 출력함.
+        Thread.sleep(500)
     }
 
 }
